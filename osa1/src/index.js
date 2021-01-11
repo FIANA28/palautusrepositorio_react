ccaee6f450086eea9101a249f31a8740c110fae7
44801231 <soinia4@gmail.com>
2021-01-11 18:28:13 +0200

    commit 1.3

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -2,52 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  // const-määrittelyt
   const course = 'Half Stack application development'
-  // const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  // const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  // const part3 = 'State of a component'
-  const exercises3 = 14
-  const Part = (props) => {
-    return (
-      <div>
-        <p>
-           {props.part} {props.exercises}
-        </p>
-      </div>
-    )
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
   }
-  const Header = () => {
-    return (
-      <div>
-        <h1>{course}</h1>
-      </div>
-    )
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
   }
-  const Content = () => {
-    return (
-    <div>
-        <Part part='Fundamentals of React' exercises={10} />
-        <Part part='Using props to pass data' exercises={7}/>
-        <Part part='State of a component' exercises={14}/>
-    </div>
-    )
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
   }
-  const Total = () => {
-    return (
-      <div>
-        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-      </div>
-    )
-  }
+
 
   return (
     <div>
-      <Header course={course} />
-      <Content />
-      <Total />
+      <h1>{course}</h1>
+      <p>{part1.name +' '+ part1.exercises}</p>
+      <p>{part2.name +' '+ part2.exercises}</p>
+      <p>{part3.name +' '+ part3.exercises}</p>
+      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
     </div>
   )
 }
